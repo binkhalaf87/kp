33c864fcb10932de9219e89000214462407036f9
binkhalaf87 <info@saudicv.org>
2026-09-17 10:17:10 +0300

    cloud storage: update dashboard/src/app/(protected)/layout.tsx

--- dashboard/src/app/(protected)/layout.tsx
+++ dashboard/src/app/(protected)/layout.tsx
@@ -1,9 +1,9 @@
 import { Sidebar, MobileNav } from "@/components/Sidebar";
-import { OneDriveSyncProvider } from "@/components/OneDriveSyncProvider";
+import { CloudSyncProvider } from "@/components/CloudSyncProvider";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <OneDriveSyncProvider>
+    <CloudSyncProvider>
       <div className="flex min-h-screen">
         <Sidebar />
         <div className="flex-1 min-w-0">
@@ -11,6 +11,6 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           <main className="max-w-[1400px] mx-auto p-4 lg:p-8">{children}</main>
         </div>
       </div>
-    </OneDriveSyncProvider>
+    </CloudSyncProvider>
   );
 }
